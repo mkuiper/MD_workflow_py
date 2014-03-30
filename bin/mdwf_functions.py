401d7f522e8547e477e252059d50b32246751f6b
Michael Kuiper <kuiper.mike@gmail.com>
2014-03-31 01:57:39 +1100

    more updates of mdwf.

--- bin/mdwf_functions.py
+++ bin/mdwf_functions.py
@@ -2,35 +2,50 @@
 #
 # MD workflow functions.   mdwf 
 
-
-# mwdf_initalize_job()
-# -Initialize the workflow directory. 
-# -specify job type and experimental design. 
-# -configure accounts and cluster 
-
-# mdwf_check_job_input()
-# -script to check the input of the current job and indicate resources needed. 
-def check_job():
-    """ -function to check the input of the current job and calculate resources required."""
-    print "-- checking job input" 
-
-
 def monitor_jobs():
     """ -function to monitor jobs status on the cluster """ 
     print "-- monitoring job status ---"
 
 def initialize_job_directories():
-    """ -function to setup and populate job directories """
-    print "-- initailizing job directories."
+    """ -function to setup job directories """
+    print "-- initializing job directories."
 
+def populate_job_directories():
+    """ -function to populate or update job directories with job scripts """
+    print "-- populating job directories."
+
+def check_job():
+    """ -function to check the input of the current job and calculate resources required."""
+    print "-- checking job input" 
+
+def benchmark():
+    """ -function to benchmark job """
+    print "-- benchmarking jobs."
+
+def start_all_jobs():
+    """ -function to start_all_jobs """
+    print "-- starting jobs."
+
+def restart_all_production_jobs():
+    """ -function to restart_all_production_jobs """
+    print "-- restarting production jobs."
+
+def recover_all_jobs():
+    """ -function to recover and restore crashed jobs """
+    print "-- recovering crashed jobs."
 
 def stop_jobs():
     """ -function to stop all jobs, -either immediately or gently."""
     print "stopping all jobs"
 
+def new_round():
+    """ -function to set up a new round of simulations."""
+    print "--- setting up new simulation round"
 
-# mdwf_benchamrk_job()
-# -taking current input set up test jobs to benchmark cluster
+def erase_all_data():
+    """ -function to erase all data for a clean start.  Use with caution!"""
+    print "erasing all data!!" 
+
 
 # mdwf_monitor_status()
 
