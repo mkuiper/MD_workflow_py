ab6a374d618bb6a32f1b684ca3b6ffffe5fe72dc
Michael Kuiper <kuiper.mike@gmail.com>
2014-04-01 11:55:38 +1100

    using json files for masterconfig.

--- bin/mdwf_functions.py
+++ bin/mdwf_functions.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
-#
 # MD workflow functions.   mdwf 
+import os
+import sys 
+import json
+
+
+def read_master_config_file():  #read variables from json format file  
+    cwd=os.getcwd()
+    if os.path.isfile('master_config_file'):
+        master_json = open('master_config_file')
+        try: 
+            master_data = json.load(master_json)
+            master_json.close()
+        except:
+            print "possible json format errors of 'master_config_file'"
+    else:
+        error = "\nCan't see 'master_config_file' in directory:" + cwd + "\n" 
+        sys.exit(error)
+    return master_data
+
 
 def monitor_jobs():
     """ -function to monitor jobs status on the cluster """ 
@@ -9,6 +27,9 @@ def monitor_jobs():
 def initialize_job_directories():
     """ -function to setup job directories """
     print "-- initializing job directories."
+    master_config_file = read_master_config_file()
+    print master_config_file
+#    print json.dumps(master_config_file)
 
 def populate_job_directories():
     """ -function to populate or update job directories with job scripts """
@@ -36,15 +57,19 @@ def recover_all_jobs():
 
 def stop_jobs():
     """ -function to stop all jobs, -either immediately or gently."""
-    print "stopping all jobs"
+    print "-- stopping all jobs"
 
 def new_round():
     """ -function to set up a new round of simulations."""
-    print "--- setting up new simulation round"
+    print "-- setting up new simulation round"
 
 def erase_all_data():
     """ -function to erase all data for a clean start.  Use with caution!"""
-    print "erasing all data!!" 
+    print "-- erasing all data!!" 
+
+def clone():
+    """ -function to clone directory without data, but preseving input files."""
+    print "-- cloning data directory!!" 
 
 
 # mdwf_monitor_status()
