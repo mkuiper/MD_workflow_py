0dd2a16a1cd8a37b89f71c02898805442cf4db16
Kuiper, Michael (Data61, Docklands GS) <Michael.Kuiper@data61.csiro.au>
2021-08-24 05:26:57 +1000

    Updated module files used on Bracewell.

--- Analysis/job_reminder.sh
+++ Analysis/job_reminder.sh
@@ -2,8 +2,18 @@
 # Job reminder script. 
 DIR = "CoV19_Spike_01"
 
-for i in {1..10}
-do 
-  printf $i 
-  less ../CoV19Spike_01/Spike_$i/namd_equil* |grep psf | grep structure
+for i in {1..30}
+ do
+   if [ "$i" -lt "10" ]; then
+     echo "Spike_0$i"
+     less ../CoV19Spike_01/Spike_0$i/namd_equil* |grep psf | grep structure
+   fi
+
+   if [ "$i" -gt "9" ]; then
+     echo "Spike_$i"
+     less ../CoV19Spike_01/Spike_$i/namd_equil* |grep psf | grep structure
+   fi
+   echo " "
+
 done
+
