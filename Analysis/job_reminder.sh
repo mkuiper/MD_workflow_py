6cbf6c13da96bf2ab9baea16db5b648f13255c60
Kuiper, Michael (Data61, Docklands GS) <Michael.Kuiper@data61.csiro.au>
2022-04-27 15:25:47 +1000

    Added better job_reminder script in /Analysis

--- Analysis/job_reminder.sh
+++ Analysis/job_reminder.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # Job reminder script. 
-DIR = "CoV19_Spike_01"
+# A simple script to help remind which jobs are running in which directories 
+# by reading the input psf file from the configuration file, namd_equilibration_script.conf 
+Red='\033[0;31m' # Red
+Lbl='\033[1;34m' # Light blue
+Yel='\033[0;33m' # Yellow
+Lgr='\033[0;37m' # Light gray
+NC='\033[0m'     # No Color
 
-for i in {1..30}
- do
-   if [ "$i" -lt "10" ]; then
-     echo "Spike_0$i"
-     less ../CoV19Spike_01/Spike_0$i/namd_equil* |grep psf | grep structure
-   fi
 
-   if [ "$i" -gt "9" ]; then
-     echo "Spike_$i"
-     less ../CoV19Spike_01/Spike_$i/namd_equil* |grep psf | grep structure
-   fi
-   echo " "
+# Edit the list of job directories here:
+for job_dir in  jobdir1 jobdir2 jobdir3  
+
+do 
+ printf "\n${Yel} /$job_dir ${NC} Input files: \n"
+
+ for d in ../$job_dir/*/ ; 
+  do 
+   (cd "$d" && printf " ${Lgr} $d: ${Lbl}  " |tr -d "\n"; less namd_eq*| grep '.psf'| grep InputFiles |
+    sed "s/structure     //" | sed "s/     ..\/..\/InputFiles\///"   ); 
+  done
 
 done
-
+printf "${NC}"
